c116777fba791ac4e2364180becd1899c26195cc
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2021-08-11 21:40:51 +0900

    1017

---
+++ CodeUp/basics/정수 1개 입력받아 3번 출력하기.cpp
@@ -0,0 +1,21 @@
+''' Case 1 '''
+#include <stdio.h>
+
+int main(){
+    int a;
+    scanf("%d", &a);
+    printf("%d %d %d", a, a, a);
+}
+
+
+''' Case 2 '''
+  #include <stdio.h>
+
+int main() {
+    int input;
+    scanf("%d", &input);
+    for(int i=0; i<3; i++) {
+        printf("%d ", input);   
+    }
+}
+
