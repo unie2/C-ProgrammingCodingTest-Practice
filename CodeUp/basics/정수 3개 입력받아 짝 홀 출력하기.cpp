ea87df56a3afbf72ab8a6ad5b75b65204a1d9419
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2021-08-13 20:47:07 +0900

    1066

---
+++ CodeUp/basics/정수 3개 입력받아 짝 홀 출력하기.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	int i, j, k;
+	scanf("%d %d %d", &i, &j, &k);
+	if(i%2 == 0) {
+		printf("%s\n", "even");
+	} else {
+		printf("%s\n", "odd");
+	}
+	if(j%2 == 0) {
+		printf("%s\n", "even");
+	} else {
+		printf("%s\n", "odd");
+	}
+	if(k%2 == 0) {
+		printf("%s\n", "even");
+	} else {
+		printf("%s\n", "odd");
+	}
+}
+
