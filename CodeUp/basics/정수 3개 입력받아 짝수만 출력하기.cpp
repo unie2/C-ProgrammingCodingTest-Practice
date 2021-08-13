b580e7f1ce015f867f5f7c9bb59f4db6847ed7a2
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2021-08-13 20:46:21 +0900

    1065

---
+++ CodeUp/basics/정수 3개 입력받아 짝수만 출력하기.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int i, j, k;
+	scanf("%d %d %d", &i, &j, &k);
+	if(i%2 == 0)
+		printf("%d\n", i);
+	if(j%2 == 0)
+		printf("%d\n", j);
+	if(k%2 == 0)
+		printf("%d\n", k);
+	
+}
+
